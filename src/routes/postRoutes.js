d264b17289865c84bd3ada5d5c38c748fc0d41dd
Abdellatif <abdellatifouhsaine1902@gmail.com>
2025-04-13 21:47:09 +0100

    Implémentation du feed personnalisé avec les posts des suivis

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { authenticate } = require('../middleware/authMiddleware');
-const { createPost, deletePost, likePost, getAllPosts, getUserPosts } = require('../controllers/postController');
+const { createPost, deletePost, likePost, getAllPosts, getUserPosts ,getFeed } = require('../controllers/postController');
 const router = express.Router();
 
 router.post('/', authenticate, createPost);
@@ -8,5 +8,6 @@ router.delete('/:postId', authenticate, deletePost);
 router.put('/:postId/like', authenticate, likePost); // Liker un post
 router.get('/user/:userId/', authenticate, getUserPosts); // Liker un post
 router.get('/', getAllPosts); // Récupérer tous les posts
+router.get("/feed", authenticate, getFeed);
 
 module.exports = router;
